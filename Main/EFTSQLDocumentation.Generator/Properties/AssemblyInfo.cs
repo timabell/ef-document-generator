46798502cf98fd4c8d6d692f091b6f7df4563859
Tim Abell <tim@timwise.co.uk>
2015-01-16 17:44:18 +0000

    Updated version and dll metadata

--- Main/EFTSQLDocumentation.Generator/Properties/AssemblyInfo.cs
+++ Main/EFTSQLDocumentation.Generator/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EFTSQLDocumentation.Generator")]
+[assembly: AssemblyTitle("EFTSQLDocumentation.Generator, see See https://github.com/timabell/ef-document-generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cyril DURAND")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EFTSQLDocumentation.Generator")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
